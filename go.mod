8ca60d739a684d18bf3c883da9963fd6ad08efc8
Xaving <rakotomamonjy.xavier@gmail.com>
2022-06-14 21:33:18 +0200

    feat: update code add go.mod and go.sum

---
+++ go.mod
@@ -0,0 +1,15 @@
+module v0
+
+go 1.17
+
+require (
+	github.com/agext/levenshtein v1.2.1 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/google/go-cmp v0.5.2 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.12.0 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/zclconf/go-cty v1.8.0 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/google/go-github/github"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/gohcl"
 	"github.com/hashicorp/hcl/v2/hclparse"
@@ -25,6 +28,10 @@ type ModuleMapping struct {
 // LoadTerraformFiles loads terraform files from a given dir.
 func main() {
 
+	////////////////
+	// first part //
+	////////////////
+
 	p := hclparse.NewParser()
 	f, diags := p.ParseHCLFile("main.tf")
 	if diags.HasErrors() {
@@ -39,10 +46,29 @@ func main() {
 		return
 	}
 
+	// Print module name and source
 	for _, m := range fm.ModuleMappings {
 		fmt.Println("Module", m.Name, m.Source)
 	}
-	// Print the structure
-	fmt.Println("RESULT", fm)
 
+	///////////////
+	//Second Part//
+	///////////////
+
+	// Parse module source Address
+	location := fm.ModuleMappings[0].Source
+	parts := strings.Split(location, "/")
+	owner := parts[3]
+	repo := strings.Split(parts[4], ".")[0]
+	// Assume that the module dir contains a main.tf and a variables.tf
+	pathv := strings.Split(parts[6], "?")[0] + "/" + "variables.tf"
+	//pathm := strings.Split(parts[6], "?")[0] + "/" + "main.tf"
+
+	// Get the file from the repo main branch
+	client := github.NewClient(nil)
+	file, _, _, _ := client.Repositories.GetContents(context.Background(), owner, repo, pathv, nil)
+	//fc, _ := *file.GetContent()
+	//fe := file.Encoding
+	//c, _ := base64.StdEncoding.DecodeString(*file.Content)
+	fmt.Println(c)
 }
